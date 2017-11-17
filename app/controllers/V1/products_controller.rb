67dc9457472447e8946f7955f3cc7658022a1398
Robert Fairchild <robertjoseph.fairchild@gmail.com>
2017-11-16 18:22:18 -0600

    create route/def

--- app/controllers/V1/products_controller.rb
+++ app/controllers/V1/products_controller.rb
@@ -4,6 +4,12 @@ class V1::ProductsController < ApplicationController
     render json: products.as_json
   end
 
+  def create
+    product = Product.new(title: params["input_name"], price: params["input_price"], image: params["input_image"])
+    product.save
+    render json: product.as_json 
+  end
+
   def show
     product_id = params["id"]
     product = Product.find_by(id:product_id)
